15e7966c5e5dbbd2452bf684984cb813aa33e74f
Josh Gummersall <josh@standardlabs.dev>
2020-05-03 10:50:01 -0700

    Clean up

--- http/http.go
+++ http/http.go
@@ -2,19 +2,50 @@ package http
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 )
+
+// GetAuthorization returns the `authType` authorization header, or an error
+func GetAuthorization(r *http.Request, authType string) (string, error) {
+	authorization := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(authorization) != 2 {
+		return "", errors.New("missing authorization header")
+	}
+
+	if !strings.EqualFold(authorization[0], authType) {
+		return "", fmt.Errorf("only %s authorization supported", authType)
+	}
+
+	return authorization[1], nil
+}
 
 // Error is a reusable, JSON serializable error message structure
 type Error struct {
 	Message string `json:"message"`
 }
 
-// ReplyJSON is a helper respond to an HTTP request with a JSON message
-func ReplyJSON(writer http.ResponseWriter, code int, response interface{}) {
+// ReplyJSON responds to an HTTP request with a JSON message
+func ReplyJSON(writer http.ResponseWriter, response interface{}, code int) {
+	writer.WriteHeader(code)
 	if err := json.NewEncoder(writer).Encode(response); err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
 	}
-	writer.WriteHeader(code)
+}
+
+// ReplyError responds to an HTTP request with an error message
+func ReplyError(writer http.ResponseWriter, err error, code int) {
+	ReplyJSON(writer, Error{Message: err.Error()}, code)
+}
+
+// ReplyInternalServerError responds to an HTTP request with an error message and 503 status
+func ReplyInternalServerError(writer http.ResponseWriter, err error) {
+	ReplyError(writer, err, http.StatusInternalServerError)
+}
+
+// ReplyBadRequestError responds to an HTTP request with an error message and 400 status
+func ReplyBadRequestError(writer http.ResponseWriter, err error) {
+	ReplyError(writer, err, http.StatusBadRequest)
 }
