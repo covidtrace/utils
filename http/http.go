41106cd02a9ef487ab4684ea8a18c67ed7727d1c
Josh Gummersall <josh@standardlabs.dev>
2020-05-03 10:50:01 -0700

    Clean up

--- http/http.go
+++ http/http.go
@@ -10,11 +10,21 @@ type Error struct {
 	Message string `json:"message"`
 }
 
-// ReplyJSON is a helper respond to an HTTP request with a JSON message
-func ReplyJSON(writer http.ResponseWriter, code int, response interface{}) {
+// ReplyJSON responds to an HTTP request with a JSON message
+func ReplyJSON(writer http.ResponseWriter, response interface{}, code int) {
 	if err := json.NewEncoder(writer).Encode(response); err != nil {
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	writer.WriteHeader(code)
+}
+
+// ReplyError responds to an HTTP request with an error message
+func ReplyError(writer http.ResponseWriter, err error, code int) {
+	ReplyJSON(writer, Error{Message: err.Error()}, code)
+}
+
+// ReplyInternalServerError responds to an HTTP request with an error message and 503 status
+func ReplyInternalServerError(writer http.ResponseWriter, err error) {
+	ReplyError(writer, err, http.StatusInternalServerError)
 }
