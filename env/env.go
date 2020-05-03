d2323bcac21ddde813883c23f812ae4a50c5592d
Josh Gummersall <josh@standardlabs.dev>
2020-05-03 10:50:01 -0700

    Clean up

--- env/env.go
+++ env/env.go
@@ -5,10 +5,21 @@ import (
 	"os"
 )
 
+// Get returns an environment variable
 func Get(key string) string {
 	return os.Getenv(key)
 }
 
+// GetDefault returns an environment variable, of a default value if it is not set
+func GetDefault(key, def string) string {
+	value := Get(key)
+	if value == "" {
+		return def
+	}
+	return value
+}
+
+// MustGet returns an environment variable, or panics if it is not set
 func MustGet(key string) string {
 	value := Get(key)
 	if value == "" {
